f2902b56e737a837b5a91cbfd4ee04de275a3bf2
Diko Mahendra <diko.dev99@gmail.com>
2023-09-17 14:06:07 +0700

    feat: add menu sidebar

---
+++ app/dashboard/constant/index.tsx
@@ -0,0 +1,49 @@
+import {
+  ICDashboard,
+  ICOrder,
+  ICPaper,
+  ICPie,
+  ICPrint,
+  ICSettings
+} from '@/src/assets/svg';
+
+export const CURRENT_TAB = 'To-Do';
+
+interface ListMenu {
+  title: string;
+  icon: React.ReactNode;
+  path: string;
+}
+
+export const listMenu: Array<ListMenu> = [
+  {
+    title: 'Dashboard',
+    icon: <ICDashboard />,
+    path: ''
+  },
+  {
+    title: 'Analytics',
+    icon: <ICPie />,
+    path: ''
+  },
+  {
+    title: 'History',
+    icon: <ICOrder />,
+    path: ''
+  },
+  {
+    title: 'To-Do',
+    icon: <ICPaper />,
+    path: ''
+  },
+  {
+    title: 'Report',
+    icon: <ICPrint />,
+    path: ''
+  },
+  {
+    title: 'Settings',
+    icon: <ICSettings />,
+    path: ''
+  }
+];
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import { CURRENT_TAB, listMenu } from './constant';
 
 export default function DashboardLayout() {
+  const [currentTab] = useState(CURRENT_TAB);
+
   return (
     <div className="">
       <div className="grid grid-cols-4 h-screen">
@@ -10,6 +15,28 @@ export default function DashboardLayout() {
               Task.
               <span className="text-primary-orange-100">IO</span>
             </h1>
+
+            <div className="px-16 mt-16">
+              {listMenu.map((item, index) => {
+                const isActive = item.title === currentTab;
+
+                const styleActive = isActive
+                  ? 'bg-primary-orange-50 text-primary-orange-100 font-semibold'
+                  : 'cursor-not-allowed';
+
+                const menuStyle = [
+                  'pl-4 mb-6 cursor-pointer flex items-center text-primary-gray-300 py-4',
+                  styleActive
+                ].join(' ');
+
+                return (
+                  <div key={index} className={menuStyle}>
+                    {item.icon}
+                    <p className="ml-4">{item.title}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
         <div className="col-span-3 bg-primary-gray-200 rounded-s-3xl rounded-b-3xl">
