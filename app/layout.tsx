8a3a5dae5b1a4929782502c486149ea79bb25114
Diko Mahendra <diko.dev99@gmail.com>
2023-09-18 05:28:25 +0700

    fix: remove unused font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import './globals.css';
-import { inter, roboto_mono } from '@/src/assets/fonts/default';
 
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
-  const fontOptimization = `${inter.variable} ${roboto_mono.variable}`;
-
   return (
-    <html lang="en" className={fontOptimization}>
+    <html lang="en">
       <body className="h-screen">{children}</body>
     </html>
   );
